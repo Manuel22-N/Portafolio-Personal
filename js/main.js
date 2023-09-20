35e649e95e6e2b0e191254d74735de4b849b2c84
Manuel22-N <134333618+Manuel22-N@users.noreply.github.com>
2023-09-19 20:07:53 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// AOS ANIMATE
+AOS.init();
+
+
+// Obtiene la URL actual
+// var currentUrl = window.location.href;
+// Selecciona los enlaces de navegación
+// var navLinks = document.querySelectorAll('.custom-nav-link');
+// Itera a través de los enlaces y agrega la clase 'active' al enlace correspondiente a la página actual
+// for (var i = 0; i < navLinks.length; i++) {
+//     var linkHref = navLinks[i].getAttribute('href');
+//     if (currentUrl.endsWith(linkHref)) {
+//         navLinks[i].classList.add('active');
+//     }
+// }
